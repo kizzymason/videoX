04f9074853499a8654e9cb4d1682acff14e286ec
Coxxs <kizzymason165@gmail.com>
2026-08-19 10:53:02 +0800

    Restore storage module barrel exports

---
+++ apps/api/src/modules/storage/index.ts
@@ -0,0 +1,12 @@
+export { StorageKeys } from './keys.js';
+export { sanitizeKey, contentTypeFor, createStorageDriver } from './driver.js';
+export type { Storage, ObjectMeta, GetResult, DriverConfig } from './driver.js';
+export {
+  getStorage,
+  listStorageProfiles,
+  createStorageProfile,
+  updateStorageProfile,
+  activateStorageProfile,
+  deleteStorageProfile,
+  testStorageProfile,
+} from './service.js';
